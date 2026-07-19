59aa2db2a0b1996920b924bf0b3e2b4763b315d9
Christian Scherlipp <c.scherlipp@web.de>
2026-07-19 11:09:19 +0200

    feat(character): added idle and long-idle animation for sharkie

--- models/character.class.js
+++ models/character.class.js
@@ -5,6 +5,8 @@ class Character extends MovableObject {
     height = 290;
     speed = 4;
     world;
+    longIdleThreshold = 8000;
+    idleTime = 0;
 
     offset = {
         top : 130,
@@ -42,7 +44,45 @@ class Character extends MovableObject {
         './assets/img/1.Sharkie/5.Hurt/1.Poisoned/2.png',
         './assets/img/1.Sharkie/5.Hurt/1.Poisoned/3.png',
         './assets/img/1.Sharkie/5.Hurt/1.Poisoned/4.png',
-        './assets/img/1.Sharkie/5.Hurt/1.Poisoned/5.png',
+        './assets/img/1.Sharkie/5.Hurt/1.Poisoned/5.png'
+    ];
+
+    IMAGES_IDLE = [
+        './assets/img/1.Sharkie/1.IDLE/1.png',
+        './assets/img/1.Sharkie/1.IDLE/2.png',
+        './assets/img/1.Sharkie/1.IDLE/3.png',
+        './assets/img/1.Sharkie/1.IDLE/4.png',
+        './assets/img/1.Sharkie/1.IDLE/5.png',
+        './assets/img/1.Sharkie/1.IDLE/6.png',
+        './assets/img/1.Sharkie/1.IDLE/7.png',
+        './assets/img/1.Sharkie/1.IDLE/8.png',
+        './assets/img/1.Sharkie/1.IDLE/9.png',
+        './assets/img/1.Sharkie/1.IDLE/10.png',
+        './assets/img/1.Sharkie/1.IDLE/11.png',
+        './assets/img/1.Sharkie/1.IDLE/12.png',
+        './assets/img/1.Sharkie/1.IDLE/13.png',
+        './assets/img/1.Sharkie/1.IDLE/14.png',
+        './assets/img/1.Sharkie/1.IDLE/15.png',
+        './assets/img/1.Sharkie/1.IDLE/16.png',
+        './assets/img/1.Sharkie/1.IDLE/17.png',
+        './assets/img/1.Sharkie/1.IDLE/18.png'
+    ];
+
+    IMAGES_LONG_IDLE = [
+        './assets/img/1.Sharkie/2.Long_IDLE/I1.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I2.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I3.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I4.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I5.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I6.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I7.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I8.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I9.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I10.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I11.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I12.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I13.png',
+        './assets/img/1.Sharkie/2.Long_IDLE/I14.png'
     ];
 
     constructor() {
@@ -50,6 +90,8 @@ class Character extends MovableObject {
         this.loadImages(this.IMAGES_SWIM);
         this.loadImages(this.IMAGES_DEAD);
         this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_LONG_IDLE);
         this.getRealFrame();
     }
 
@@ -73,17 +115,28 @@ class Character extends MovableObject {
             this.y += this.speed * factor;
         }
         this.world.camera_x = -this.x + 50;
-        this.applyGravity(deltaTime);
         this.getRealFrame();
-
+        
+        let isMoving = this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.DOWN;
+        if (isMoving) {
+            this.idleTime = 0;
+        } else {
+            this.idleTime += deltaTime;
+        }
+        
         this.animationTimer += deltaTime;
         if(this.animationTimer > 150){
             if (this.isDead()) {
                 this.playAnimation(this.IMAGES_DEAD);
             } else if (this.isHurt()) {
                 this.playAnimation(this.IMAGES_HURT);
-            } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.DOWN) {
+            } else if (isMoving) {
                 this.playAnimation(this.IMAGES_SWIM);
+            }else if (this.idleTime > this.longIdleThreshold) {
+                this.playAnimation(this.IMAGES_LONG_IDLE);
+                this.applyGravity(deltaTime);
+            } else {
+                this.playAnimation(this.IMAGES_IDLE);
             }
             this.animationTimer = 0;
         }
